651e2c9c08c0959365b9088cb0aca361f7fb6527
Ecthor Silva <EcthorSilva@outlook.com>
2023-05-09 15:11:45 -0300

    Modulo 5 - Aula 2

--- js/custom.js
+++ js/custom.js
@@ -20,4 +20,41 @@ $(document).ready(function(){ // jQuery( function($){
         alert('Produto esgotado');
     });
 
+    /*
+     * Callback
+     *
+     * entendendo ações que começam ao termino de outra
+     */
+    // $('.featured-item:nth(1)')
+    //     .hide(2000, function(){
+    //         // este é o callback
+    //         console.log($(this).find('h4').text() + ' esgotado');
+    //     })
+    //     .show(2000, function(){
+    //         console.log($(this).find('h4').text() + ' em estoque');
+    //     })
+
+    /*
+     * Animações
+     */
+    // const duracao = 1000; // equivale a 1 segundo
+    // $('.featured-item:nth(0)')
+    //     .hide(duracao)
+    //     .show(duracao)
+    //     .fadeOut(duracao)
+    //     .fadeIn(duracao)
+
+    const duracao = 1000; // equivale a 1 segundo
+    $('#form-submit').on('click', function(e){
+        e.preventDefault()
+
+        if($('#email').val().length != ''){
+            $('#email').animate({
+                opacity: "toggle",
+                top: "-50"
+            }, duracao, function(){
+                console.log($(this).val());
+            });
+        }
+    })
 })
